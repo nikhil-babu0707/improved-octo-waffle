ce55305678ded38ca907282f826d2e29db2b55be
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2022-01-06 13:43:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,41 @@ function speak() {
     var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
     synth.speak(utterThis);
 }
+
+function check() {
+    image = document.getElementById("capture_image");
+    classifier.classify(image, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+        if (result[0].label = "super") {
+            document.getElementById("update_emoji").innerHTML = "&#128076";
+        }
+        if (result[0].label = "thumbs up") {
+            document.getElementById("update_emoji").innerHTML = "&#128077";
+        }
+        if (result[0].label = "chears") {
+            document.getElementById("update_emoji").innerHTML = "&#9996";
+        }
+
+        if (result[1].label = "super") {
+            document.getElementById("update_emoji").innerHTML = "&#128076";
+        }
+        if (result[1].label = "thumbs up") {
+            document.getElementById("update_emoji").innerHTML = "&#128077";
+        }
+        if (result[1].label = "chears") {
+            document.getElementById("update_emoji").innerHTML = "&#996";
+        }
+    }
+
+}
